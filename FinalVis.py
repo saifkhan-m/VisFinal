a6422f2a96bd3790cdebac8898398a6c0932b1b4
Saif Khan <Saif Khan>
2019-08-30 18:54:44 +0200

    added initial function

--- FinalVis.py
+++ FinalVis.py
@@ -2,20 +2,20 @@ from matplotlib import pyplot as plt
 import cv2
 import Utils
 import MarchingSquares
-import numpy as np
 
-img = cv2.imread('Mars_MGS_MOLA_DEM_mosaic_global_1024.jpg',0)#mars_mercator_2_512x512
+def initVis(img,totalBins):
+    quantizedImage=Utils.binImage(img,totalBins)
+    marchedImage=MarchingSquares.marchingSquares(quantizedImage,2)
+    quantizedImageRGB = cv2.cvtColor(marchedImage,cv2.COLOR_GRAY2RGB)
+    shadedImage=Utils.shadeImage1(quantizedImageRGB,marchedImage,totalBins)
+
+    return shadedImage
+
+
+img = cv2.imread('Mars_MGS_MOLA_DEM_mosaic_global_1024.jpg',0)
 #img = cv2.imread('mars_mercator_2_512x512.png',0)
-plt.imshow(img)
 totalBins=16
-#plt.show()
-quantizedImage=Utils.binImage(img,totalBins)
-#cv2.imshow('image',quantizedImage)
-marchedImage=MarchingSquares.marchingSquares(quantizedImage,2)
-
-quantizedImageRGB = cv2.cvtColor(marchedImage,cv2.COLOR_GRAY2RGB)
-shadedImage=Utils.shadeImage1(quantizedImageRGB,marchedImage,totalBins)
-#hist = cv2.calcHist([quantizedImage], [0], None, [256], [0, 256])
+shadedImage=initVis(img,totalBins)
 cv2.imwrite('Elevation Map '+str(totalBins)+'.png',shadedImage)
 
 plt.imshow(cv2.cvtColor(shadedImage, cv2.COLOR_BGR2RGB))
@@ -23,7 +23,5 @@ plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
 
 #cv2.imshow('image',shadedImage)
-
-#cv2.imshow('image',shadedImage)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
